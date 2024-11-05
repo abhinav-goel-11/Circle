d30745d774939e9ce92e7d1d00c32d081170e32f
abhinav <abhinav@hood.live>
2024-11-05 16:20:14 +0530

    safari fix

--- src/components/bg-gradient.tsx
+++ src/components/bg-gradient.tsx
@@ -5,7 +5,7 @@ export default function BgGradient() {
   return (
     <div className="fixed -z-50 min-h-screen w-full bg-black ">
       {/*_______Green Blob_______*/}
-      <div className=" bg-accent rounded-full w-[588px] sm:w-[999px] h-[572px] sm:h-[971px] absolute top-1/2 left-1/2 -translate-x-1/2 blur-[150px] opacity-40 sm:opacity-50 sm:blur-[254px]" />
+      <div className=" bg-accent rounded-full w-[588px] sm:w-[999px] h-[572px] sm:h-[971px] absolute top-1/2 left-1/2 -translate-x-1/2 blur-[150px] opacity-40 sm:opacity-50 sm:blur-[254px] will-change-transform" />
       {/*_______Noise ________*/}
       <Image
         src={Noise}
