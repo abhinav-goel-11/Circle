75dd76b8ebac90155cca8403c86fd996410c35c9
abhinav <abhinav@hood.live>
2025-01-23 15:45:03 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       </header>
       <section className=" flex items-center justify-center flex-1 ">
         <div className="flex items-center flex-col gap-14 sm:gap-8">
-          <h2 className="text-white/60 text-xl sm:text-[1.75rem] font-normal text-center w-[272px] sm:w-[380px] leading-[1.3]">
-            See what&apos;s happening locally within 15 km radius.
-          </h2>
+          <h1 className="text-white/60 text-xl sm:text-[1.75rem] font-normal text-center w-[272px] sm:w-[380px] leading-[1.3]">
+            see what&apos;s happening locally within 15 km radius.
+          </h1>
           <div className="flex flex-col gap-y-6 sm:flex-row items-center gap-x-8 h-[66px] sm:h-[72px]">
             <Image src={App} alt="app store" className="h-full w-auto" />
             <Image src={Play} alt="Play store" className="h-full w-auto" />
